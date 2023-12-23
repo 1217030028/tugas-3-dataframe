017eabf2d7a211bf9ed402b1c369f148e111ba79
Nurul Jamilah <144510018+1217030028@users.noreply.github.com>
2023-12-23 11:20:11 +0700

    Add files via upload

---
+++ tugas 3 part (2).py
@@ -0,0 +1,11 @@
+import numpy as np
+
+# Membuat array x menggunakan NumPy
+x = np.arange(0, 30) 
+
+# Menghitung nilai sinus dari setiap elemen dalam array x
+y = np.sin(x)
+
+# Menampilkan x dan y
+print("x:", x)
+print("y:", y)
